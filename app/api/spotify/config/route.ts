35bec121e3b1b7d8440bb1a83f7d0c8d11880cc2
Francesco Eramo <francescoeramo4@gmail.com>
2026-08-14 13:01:34 +0200

    Remove Spotify token handling

--- app/api/spotify/config/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-import { getRequestOrigin, getSpotifyClientId, noStoreJson } from "../_lib";
-
-export const dynamic = "force-dynamic";
-
-export function GET(request: NextRequest) {
-  return noStoreJson({ configured: Boolean(getSpotifyClientId() && getRequestOrigin(request)) });
-}
